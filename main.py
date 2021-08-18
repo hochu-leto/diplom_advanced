ff176f6513974c196849a1756d07f97c7158a06b
Тимофей <porno_mail@bk.ru>
2021-08-18 22:18:01 +0400

    другие люди по след запросу

--- main.py
+++ main.py
@@ -32,14 +32,13 @@ with open(token_file) as gitignore:
 vk = vk_api.VkApi(token=token)
 user_vk = vk_api.VkApi(token=TOKEN)
 longpoll = VkLongPoll(vk)
-
+offset = 0
 
 def write_msg(user_id, message, attach):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'attachment': attach, 'random_id': randrange(10 ** 7)})
 
 
 for event in longpoll.listen():
-    pprint(event)
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
             request = event.text
@@ -64,44 +63,49 @@ for event in longpoll.listen():
             else:
                 write_msg(event.user_id, 'Да что ты такое??!\n Иди, с полом своим сначала определись', '')
 
-            response = user_vk.method('users.search', {'q': ' ', 'count': 20, 'fields': 'bdate', 'has_photo': 1,
-                                                       'status': 6,
-                                                       'country': user_country, 'city': user_city, 'sex': find_sex,
-                                                       'age_from': find_age_from, 'age_to': user_age + delta_years})
 
             members = []
-            for mem in response['items']:
-                print(mem['first_name'] + ' - ' + str(mem['is_closed']) + ' - ' + str(mem['id']) + ' - ' + mem['bdate'])
-                member = {}
-                if not mem['is_closed']:
-                    photo_response = user_vk.method('photos.get', {'owner_id': mem['id'], 'count': 100, 'extended': 1,
-                                                                   'album_id': 'wall', 'photo_sizes': 1})
-                    search_photo = []
-                    for photos in photo_response['items']:
-                        add_photo = {}
-                        likomments = (photos['likes']['count'] + 1) * (photos['comments']['count'] + 1)
-                        for photos_sizes in photos['sizes']:
-                            if photos_sizes['type'] == 'p':
-                                # pprint(photos)
-                                add_photo['url'] = photos_sizes['url']
-                                add_photo['likomments'] = likomments
-                                add_photo['photo_id'] = photos['id']
-                                break
-                        if add_photo:
-                            search_photo.append(add_photo)
-                    search_photo.sort(key=itemgetter('likomments'), reverse=True)
-                    if len(search_photo) > 2:
-                        member['name'] = mem['first_name']
-                        member_bdate = datetime.strptime(mem['bdate'], '%d.%m.%Y').date()
-                        today = datetime.today()
-                        member['age'] = today.year - member_bdate.year - (
-                                    (today.month, today.day) < (member_bdate.month, member_bdate.day))
-                        member['photos'] = search_photo[:3]
-                        member['url'] = 'https://vk.com/id' + str(mem['id'])
-                        member['mem_id'] = mem['id']
-                        attach = ''
-                        for photo in member['photos']:
-                            attach += 'photo' + str(mem['id']) + '_' + str(photo['photo_id']) + ','
-                        write_msg(event.user_id, mem['first_name'] + ' ' + str(member['age']) + ' лет\n'
-                                  + member['url'], attach)
-                        # pprint(member)
+
+            while len(members) < 10:
+                print(len(members))
+                response = user_vk.method('users.search', {'q': ' ', 'count': 5, 'fields': 'bdate', 'has_photo': 1,
+                                                           'status': 6, 'offset': offset,
+                                                           'country': user_country, 'city': user_city, 'sex': find_sex,
+                                                           'age_from': find_age_from, 'age_to': user_age + delta_years})
+
+                for mem in response['items']:
+                    offset += 1
+                    print(mem['first_name'] + ' - ' + str(mem['is_closed']) + ' - ' + str(mem['id']) + ' - ' + mem['bdate'])
+                    member = {}
+                    if not mem['is_closed']:
+                        photo_response = user_vk.method('photos.get', {'owner_id': mem['id'], 'count': 100, 'extended': 1,
+                                                                       'album_id': 'wall', 'photo_sizes': 1})
+                        search_photo = []
+                        for photos in photo_response['items']:
+                            add_photo = {}
+                            likomments = (photos['likes']['count'] + 1) * (photos['comments']['count'] + 1)
+                            for photos_sizes in photos['sizes']:
+                                if photos_sizes['type'] == 'p':
+                                    # pprint(photos)
+                                    add_photo['url'] = photos_sizes['url']
+                                    add_photo['likomments'] = likomments
+                                    add_photo['photo_id'] = photos['id']
+                                    break
+                            if add_photo:
+                                search_photo.append(add_photo)
+                        search_photo.sort(key=itemgetter('likomments'), reverse=True)
+                        if len(search_photo) > 2:
+                            member['name'] = mem['first_name']
+                            member_bdate = datetime.strptime(mem['bdate'], '%d.%m.%Y').date()
+                            today = datetime.today()
+                            member['age'] = today.year - member_bdate.year - (
+                                        (today.month, today.day) < (member_bdate.month, member_bdate.day))
+                            member['photos'] = search_photo[:3]
+                            member['url'] = 'https://vk.com/id' + str(mem['id'])
+                            member['mem_id'] = mem['id']
+                            attach = ''
+                            for photo in member['photos']:
+                                attach += 'photo' + str(mem['id']) + '_' + str(photo['photo_id']) + ','
+                            write_msg(event.user_id, mem['first_name'] + ' ' + str(member['age']) + ' лет\n'
+                                      + member['url'], attach)
+                            members.append(member)добавил
